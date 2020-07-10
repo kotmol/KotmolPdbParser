4644f89cbf740144326e58d024573403bc62961c
Jim Andreas <jim@jimandreas.com>
2020-07-10 07:43:49 +0200

    testing fixes

---
+++ kotmolpdbparser/src/test/java/com/kotmol/pdbParser/AFailedTest.kt
@@ -0,0 +1,12 @@
+package com.kotmol.pdbParser
+
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+
+internal class AFailedTest {
+    @Test
+    @DisplayName("This Test Fails by design")
+    fun failTest() {
+        assert(false)
+    }
+}
